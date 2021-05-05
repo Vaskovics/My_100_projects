68a31a2c863bd649254b55930a6b8489ded997ea
Vaskovics <78701608+Vaskovics@users.noreply.github.com>
2021-05-05 11:15:53 +0100

    Add files via upload

---
+++ 01_Band_name_generator/01_Band_name_generator.py
@@ -0,0 +1,7 @@
+
+print("Hello my fried!")
+
+city = input("What is name of the city you grew up in?\n")
+pet = input("What is you first pet's name is?\n")
+
+print("You band name could be " + city + " " + pet)
