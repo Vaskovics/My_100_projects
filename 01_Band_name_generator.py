53f9eb749b115ba6e9be939048a962a13669ac76
Vaskovics <78701608+Vaskovics@users.noreply.github.com>
2021-05-05 11:16:01 +0100

    Delete 01_Band_name_generator.py

--- 01_Band_name_generator.py
+++
@@ -1,7 +0,0 @@
-
-print("Hello my fried!")
-
-city = input("What is name of the city you grew up in?\n")
-pet = input("What is you first pet's name is?\n")
-
-print("You band name could be " + city + " " + pet)
